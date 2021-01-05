c5cc2243c27a48d935ada718e22e8d040f3d685c
THEOLUCHIJUNE <69516167+THEOLUCHIJUNE@users.noreply.github.com>
2021-01-05 23:57:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "";
+            string LastName = "";
+            int Age = 0;
+
+            Console.Write("Please enter your first name: ");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + FirstName);
+            
+            Console.Write("Please enter your last name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + LastName);
+        }
+    }
+}
